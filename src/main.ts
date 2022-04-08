7389026b0d6331ffd294a3d5ee5495bf0d2d61ff
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2022-04-08 11:46:07 +0100

    output is in commonjs

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import express, { Request, Response } from "express";
 
-import { hello } from '@functions/fn';
+import { hello } from "@functions/fn";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response): Response => {
-hello('this is hot reloading and compiling');
-return res.send("hello world");
+  hello("this is hot reloading and compiling");
+  return res.send("hello world");
 });
 
 app.listen(3000);
